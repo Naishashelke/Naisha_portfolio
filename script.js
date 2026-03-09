ff6096699db107ac03b47bc734dbaf79681852a5
Naishashelke <158588061+Naishashelke@users.noreply.github.com>
2026-03-09 20:46:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,33 @@
-/* CUSTOM CURSOR */
-
 const cursor=document.createElement("div")
+
 cursor.classList.add("cursor")
+
 document.body.appendChild(cursor)
 
 document.addEventListener("mousemove",(e)=>{
-cursor.style.left=e.pageX+"px"
-cursor.style.top=e.pageY+"px"
-})
 
-/* CLICK SPARKLE EFFECT */
+cursor.style.left=e.pageX+"px"
+
+cursor.style.top=e.pageY+"px"
+
+})
 
 document.addEventListener("click",(e)=>{
 
-let sparkle=document.createElement("span")
+let sparkle=document.createElement("div")
 
 sparkle.style.position="absolute"
 sparkle.style.left=e.pageX+"px"
 sparkle.style.top=e.pageY+"px"
-sparkle.style.width="8px"
-sparkle.style.height="8px"
+sparkle.style.width="10px"
+sparkle.style.height="10px"
 sparkle.style.background="#b57edc"
 sparkle.style.borderRadius="50%"
-sparkle.style.pointerEvents="none"
-sparkle.style.animation="sparkle 0.6s linear"
 
 document.body.appendChild(sparkle)
 
 setTimeout(()=>{
 sparkle.remove()
-},600)
-
-})
-
-/* SCROLL ANIMATION */
-
-window.addEventListener("scroll",()=>{
-
-document.querySelectorAll(".card").forEach(card=>{
-
-let position=card.getBoundingClientRect().top
-let screen=window.innerHeight
-
-if(position<screen-100){
-card.style.opacity="1"
-card.style.transform="translateY(0)"
-}
-
-})
+},500)
 
 })
